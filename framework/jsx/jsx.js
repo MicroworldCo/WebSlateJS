e1433f4c7f1074f3bfc340ccf860eda17840c1e6
coder <coder@penguin>
2021-01-14 10:31:38 -0500

    finished jsx implemention

--- framework/jsx/jsx.js
+++ framework/jsx/jsx.js
@@ -5,12 +5,17 @@ var components = split(config.split(',').toString().replace(",",""),'\n')[0].spl
 var dataProviderName = split(config.split(',').toString().replace(",",""),'\n')[1]
 var componentsLength = components.length;
 var componentsFiles = {};
-for (var counter = 0;componentsLength+1>counter;counter++;){
-    var component = read(`http://localhost:3000/components/${components[counter]}`);
-    componentsFiles[components[counter]] = component;
-};
 var dataProvider = import(`localhost:3000/dataProviders/${dataProviderName}`);
 var componethelper = new componentLib.component(components)
-for(var counter = 0; componentsFiles.size>counter;counter++;){
-    
-}
+var html = []
+
+for (var counter = 0;componentsLength+1>counter;counter++){
+    var component = read(`http://localhost:3000/components/${components[counter]}`);
+    componentsFiles.set(components[counter],component);
+};
+for(var counter = 0; componentsFiles.size>counter;counter++){
+    var id = componentgetcomponentids(componentFiles.get(components[counter]));
+    var props = dataProvider.getprops(id);
+    var componenthtml = componentHtml(componentFiles.get(components[counter]),props);
+    html.push(componenthtml);
+};
