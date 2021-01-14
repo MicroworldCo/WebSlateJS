0e0e65acaea973e5d37c892f679bcd5d5f88791d
coder <coder@penguin>
2021-01-14 12:02:26 -0500

    eslint added

--- assets/assets.js
+++ assets/assets.js
@@ -1,4 +1,4 @@
-class assets{
+export class assets{
     read(Path){
         var xhr = new XMLHttpRequest();
         xhr.onreadystatechange = function() {
@@ -9,49 +9,7 @@ class assets{
         }
         xhr.open('GET', Path);
         xhr.send();
+        // eslint-disable-next-line no-undef
         return window[res];
     }
-    split(string,splitter){
-        
-        separatedArray = []; 
- 
-        // index of end of the last string  
-        let previousIndex = 0; 
- 
-        for(i = 0; i < string.length; i++) { 
- 
-        // check the character for a comma 
-        if (string[i] == splitter) { 
- 
-            // split the string from the last index 
-            // to the comma 
-            separated = string.slice(previousIndex, i); 
-            separatedArray.push(separated); 
- 
-            // update the index of the last string 
-            previousIndex = i + 1; 
-        }
-         
-    
- 
-        // push the last string into the array 
-        separatedArray.push(originalString.slice(previousIndex, i)); 
-        }
-        return separatedArray;
-    }
-    componentHtml(jsx,props){
-        var jsxformat = jsx.replaceAll(/^(?=.*{)(?=.*}).*$/im,'|');
-        var propsList = jsxformat.split("|");
-        for(var i = 0;i<propsList.length+1;){
-            if(propsList[i].includes("props")){
-                var prop = propsList[i].split('.');
-                var propProperty = prop[1];
-                var propVal = props[propProperty];
-                propsList[i] = propVal;
-            };
-        };
-        var commaHtml = propList.join();
-        var html = commaHtml.replaceAll(",","");
-        return html;
-    };
 }
--- framework/components.js
+++ framework/components.js
@@ -1,7 +1,7 @@
-class component{
+export class component{
     constructor(components){
         this.components = components;
-    };
+    }
     getcomponentids(jsx){
         var ids = [];
         var markup = jsx.replaceAll('<','|').replaceAll('>','|').split("|");
@@ -11,18 +11,33 @@ class component{
             if(markup[counter] == this.components[component]){
                 var id = this.getid(markup[counter]);
                 ids.push(id);
-            };
-        };
+            }
+        }
         return ids;
-    };
+    }
     getid(html){
         var counter = 0;
         var markup = html.toLowerCase().split(" ");
         for(counter<markup.length;counter++;){
             if(markup[counter] == 'id'){
                 var id = markup[counter+2];
-            };
-        };
+            }
+        }
         return id;
-    };
-};
+    }
+    componentHtml(jsx,props){
+        var jsxformat = jsx.replaceAll(/^(?=.*{)(?=.*}).*$/im,'|');
+        var propsList = jsxformat.split("|");
+        for(var i = 0;i<propsList.length+1;){
+            if(propsList[i].includes("props")){
+                var prop = propsList[i].split('.');
+                var propProperty = prop[1];
+                var propVal = props[propProperty];
+                propsList[i] = propVal;
+            }
+        }
+        var commaHtml = propsList.join();
+        var html = commaHtml.replaceAll(",","");
+        return html;
+    }
+}
